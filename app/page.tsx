c8a19378a06fb296c70e7163d7dfdd7ed99ba006
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-13 18:17:50 +0300

    key

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 <h1>TEST</h1>
 
-<div>{w.props.initialContacts.map(m=><h2>{m.firstName}</h2>)}</div>
+<div>{w.props.initialContacts.map(m=><h2 key={m.id}>{m.firstName}</h2>)}</div>
     </main>
   )
 }
